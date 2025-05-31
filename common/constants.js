cd876f8853034b4dc0d282fc00c4037c21792d5f
moneybaggjoe <code.dieselbot+0000@gmail.com>
2025-05-31 17:32:49 -0400

    add scope docs + values

---
+++ common/constants.js
@@ -0,0 +1,9 @@
+exports = module.exports = {
+    scope_docs: "https://docs.genius.com/#/authentication-h1:~:text=client%2Donly%20application%3F-,Available%20Scopes,-Access%20tokens%20can",
+    available_scopes: new Map([
+         ['me', true],
+         ['create_annotation', true],
+         ['manage_annotation', true],
+         ['vote', true]
+    ])
+}
